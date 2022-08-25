9574ba50e76fae485b104309d461b30974ffaba6
Sergey <hojalata@mail.ru>
2022-08-25 16:16:20 +0300

    Выполнил задачу 41

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 41.
+
+Console.Write("Введите разрад числа Фибоначчи: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+array[0] = 0;
+array[1] = 1;
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+
+for (int i = 2; i < size; i++)
+{
+    int nextIndex = array[0] + array[1];
+    array[0] = array[1];
+    array[1] = nextIndex;
+    Console.WriteLine(nextIndex);
+}
